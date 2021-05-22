c10966872d1076ba5ed55b8b027093f6d036fa47
Steve Mbiele <stevembiele@gmail.com>
2021-05-21 23:58:12 -0400

    base commit

---
+++ src/app/mocks/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'All Might' },
+  { id: 2, name: 'Endeavor' },
+  { id: 3, name: 'Hawks' },
+  { id: 4, name: 'Best Jeanist' },
+  { id: 5, name: 'edgeshot' },
+  { id: 6, name: 'Mirko' },
+  { id: 7, name: 'Crust' },
+  { id: 8, name: 'Kamui Woods' },
+  { id: 9, name: 'Wash' },
+  { id: 10, name: 'Yoroi Musha' },
+];
